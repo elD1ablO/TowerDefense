743dc6a27a8130d98af25788d1900398f19265d1
Maksym <eldiablo@ukr.net>
2022-01-04 17:52:30 +0200

    tower attack

---
+++ Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHP = 5;
+    int currentHP = 0;
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        currentHP--;
+        if(currentHP <= 0) 
+            Destroy(gameObject);
+    }
+}
--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] bool isPlaceable;
+    [SerializeField] GameObject towerPrefab;
     private void OnMouseDown()
     {
         if (isPlaceable)
         {
-            Debug.Log(transform.name);
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
         }
         
     }

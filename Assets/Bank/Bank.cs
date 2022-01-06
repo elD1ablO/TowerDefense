35f00b1d05a1e07e66231c008e8ce5c1b15ffc6a
Maksym <eldiablo@ukr.net>
2022-01-06 14:44:46 +0200

    Font and goldText UI

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,31 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
-    [SerializeField] int startingBalance = 100;
+    [SerializeField] int startingBalance = 150;
     [SerializeField ]int currentBalance;
+    
+    [SerializeField] TextMeshProUGUI goldText;
+
+
     public int CurrentBalance { get { return currentBalance; } }
 
     private void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
-        if(currentBalance < 0)
+        if (currentBalance < 0)
         {
             ReloadScene();
         }
+        
+    }
+
+    void UpdateDisplay()
+    {
+        goldText.text = "Gold: " + currentBalance.ToString();
     }
 
     void ReloadScene()

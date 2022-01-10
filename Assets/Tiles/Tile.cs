02e81c3a5e9d5349c19904568143468b67a7b18d
Maksym <eldiablo@ukr.net>
2022-01-10 14:52:53 +0200

    blocking of path

---
+++ Assets/Tiles/Tile.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] Tower towerPrefab;
+    
+    [SerializeField] bool isPlaceable;
+    public bool IsPlaceable { get { return isPlaceable; } }
+    
+    GridManager gridManager;
+    Vector2Int coordinates = new Vector2Int();
+    private void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+    }
+    private void Start()
+    {
+        if(gridManager != null)
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+
+            if (!IsPlaceable)
+            {
+                gridManager.BlockNode(coordinates);
+            }
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isPlaceable)
+        {
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;
+        }
+        
+    }
+}
